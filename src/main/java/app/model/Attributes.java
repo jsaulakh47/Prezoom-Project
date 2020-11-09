7fc5d7a1cd3b6284db1cfe110d57245f735b605d
Nana Kwame <9naquame@gmail.com>
2020-11-09 19:33:16 -0330

    added Attrbutes and Objects model

---
+++ src/main/java/app/model/Attributes.java
@@ -0,0 +1,13 @@
+package app.model;
+
+import app.interfaces.AttributesI;
+
+public class Attributes implements AttributesI {
+
+    @Override
+    public String get() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
