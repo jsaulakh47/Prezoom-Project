e30b3410f382dca1d9fba0837f21e88362c1798c
JAGBIR SINGH AULAKH <jagbir.aulakh96@gmail.com>
2020-11-11 16:01:03 -0330

    adding interface for transition

---
+++ src/main/java/app/model/CameraTrigger.java
@@ -0,0 +1,12 @@
+package app.model;
+
+import app.interfaces.TransitionI;
+
+public class CameraTrigger implements TransitionI {
+
+    @Override
+    public void trigger() {
+        // TODO Auto-generated method stub
+    }
+    
+}
