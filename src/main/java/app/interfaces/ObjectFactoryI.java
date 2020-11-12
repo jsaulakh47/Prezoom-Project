442d8457da003fd537dda77b1d66cba1171cd036
JAGBIR SINGH AULAKH <jagbir.aulakh96@gmail.com>
2020-11-12 01:59:09 -0330

    add minor changes again

--- src/main/java/app/interfaces/ObjectFactoryI.java
+++ src/main/java/app/interfaces/ObjectFactoryI.java
@@ -2,13 +2,13 @@ package app.interfaces;
 
 public interface ObjectFactoryI {
 
-    public ObjectsI makecircle ();
-    public ObjectsI makepolygon ();
-    public ObjectsI makeline ();
-    public ObjectsI makerectangular ();
-    public ObjectsI makeeclipse ();
-    public ObjectsI maketext ();
-    public ObjectsI maketextArea ();
-    public ObjectsI makeimage();
+    public ObjectsI makeCircle ();
+    public ObjectsI makePolygon ();
+    public ObjectsI makeLine ();
+    public ObjectsI makeRectangular ();
+    public ObjectsI makeEclipse ();
+    public ObjectsI makeText ();
+    public ObjectsI makeTextArea ();
+    public ObjectsI makeImage();
 
 }
