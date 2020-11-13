209c7a5fed6d81ce2a65b118cec2435b4449b1ec
Vraj Patel <vspatel@mun.ca>
2020-11-13 12:21:40 -0330

    adding interaction.java class

---
+++ src/main/java/app/api/Interaction.java
@@ -0,0 +1,17 @@
+package app.api;
+
+import app.model.State;
+
+public class Interaction {
+    public int addState()
+    {
+        State state = new State();
+        return state.getId();
+
+    }
+    // public void addObject();
+    // public void addAttributes();
+    
+
+}
+
