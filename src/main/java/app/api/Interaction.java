610574394391e442086db030b6c485550b716dda
Nana Kwame <9naquame@gmail.com>
2020-11-15 17:33:29 -0330

    update to gui and model interaction

--- src/main/java/app/api/Interaction.java
+++ src/main/java/app/api/Interaction.java
@@ -1,13 +1,16 @@
 package app.api;
 
 import java.util.List;
+import java.util.Map;
 
+import app.interfaces.ObjectFactoryI;
+import app.interfaces.ObjectsI;
 import app.model.Sheet;
 import app.model.States;
+import app.model.objects.ObjectFactory;
 
 public class Interaction {
-    public static int addState()
-    {
+    public static int addState() {
         return Sheet.getInstance().addState();
     }
 
@@ -15,7 +18,29 @@ public class Interaction {
         return Sheet.getInstance().getStates();
     }
 
-    // public static List<States> addObject(int stateId) {
-    //     return Sheet.getInstance().adObject();
-    // }
+    public static Map<String, String> createObject(String type, String x, String y) {
+        
+        ObjectFactoryI factory = new ObjectFactory();
+        ObjectsI object;
+        if ("Circle".equals(type)) {
+            object = factory.makeCircle(x, y);
+            return object.getAttributes();
+        } else if ("Image".equals(type)) {
+            object = factory.makeCircle(x, y);
+            return object.getAttributes();
+        } else if ("Line".equals(type)) {
+            object = factory.makeLine(x, y);
+            return object.getAttributes();
+        } else if ("Text".equals(type)) {
+            object = factory.makeText(x, y);
+            return object.getAttributes();
+        }  else if ("TextArea".equals(type)) {
+
+        } else {
+            object = factory.makeRectangle(x, y);
+            return object.getAttributes();
+        }
+        
+        return null;
+    }
 }
