f08d9f0df6293365e15b82186dfe84ceab62ff64
Disha Bodiwala <dkbodiwala@mun.ca>
2020-11-15 16:53:15 -0330

    ObjectFactory Implemented

--- src/main/java/app/interfaces/ObjectFactoryI.java
+++ src/main/java/app/interfaces/ObjectFactoryI.java
@@ -2,10 +2,10 @@ package app.interfaces;
 
 public interface ObjectFactoryI {
 
-    public ObjectsI makeCircle ();
-    public ObjectsI makeLine ();
-    public ObjectsI makeRectangle ();
-    public ObjectsI makeText ();
-    public ObjectsI makeTextArea ();
-    public ObjectsI makeImage();
+    public ObjectsI makeCircle (String x, String y);
+    public ObjectsI makeLine (String x, String y);
+    public ObjectsI makeRectangle (String x, String y);
+    public ObjectsI makeText (String x, String y);
+    public ObjectsI makeTextArea (String x, String y);
+    public ObjectsI makeImage(String x, String y);
 }
--- src/main/java/app/model/objects/ObjectFactory.java
+++ src/main/java/app/model/objects/ObjectFactory.java
@@ -2,34 +2,43 @@ package app.model.objects;
 
 import app.interfaces.ObjectFactoryI;
 import app.interfaces.ObjectsI;
+import app.model.objects.Circle;
+import app.model.objects.Line;
+import app.model.objects.Rectangle;
+import app.model.objects.PlainText;
+import app.model.objects.TextArea;
+import app.model.objects.Image;
+
 
 public class ObjectFactory implements ObjectFactoryI{
-    public ObjectsI makeCircle() {
-        return null;
+    public ObjectsI makeCircle(String x, String y) {
+        Circle circle = new Circle(x , y);
+        return circle;
     }
 
-    public Objects makeLine() {
-        return null;
-    }
-    
-    public ObjectsI makeRectangle() {
-        return null;
+    public ObjectsI makeLine(String x, String y) {
+        Line line = new Line(x , y);
+        return line;
     }
     
-    public Objects makeText() {
-        return null;
-
+    public ObjectsI makeRectangle(String x, String y) {
+        Rectangle rectangle = new Rectangle(x , y);
+        return rectangle;
     }
     
-    public Objects makeTextArea() {
-        return null;
-
+    public ObjectsI makeText(String x, String y) {
+        PlainText text = new PlainText(x , y);
+        return text;
     }
     
-    public Objects makeImage() {
-        return null;
-
-
+    public ObjectsI makeTextArea(String x, String y) {
+        TextArea textArea = new TextArea(x , y);
+         return textArea;
+    }
+    
+    public ObjectsI makeImage(String x, String y) {
+        Image image = new Image(x , y);
+        return image;
     }
 
 }
