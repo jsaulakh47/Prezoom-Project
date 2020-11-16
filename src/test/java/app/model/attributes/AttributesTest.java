d1bee2fdb3bac7d9c90df890fd19139229cf99bd
disha bodiwala <dkbodiwala@mun.ca>
2020-11-15 23:20:01 -0700

    Added AttributesTest

---
+++ src/test/java/app/model/attributes/AttributesTest.java
@@ -0,0 +1,20 @@
+package app.model.attributes;
+
+import app.model.objects.ObjectFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class AttributesTest {
+
+    Attributes attributes;
+
+    @BeforeAll
+    public void setup() {
+        attributes = new Attributes("", "");
+    }
+
+    @Test
+    public void testSetData() {
+        // TODO: Add test case code here for setData() method.
+    }
+}
