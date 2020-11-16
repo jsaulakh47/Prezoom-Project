0a811b79300f859d675a8de143c8e9862035209a
disha bodiwala <dkbodiwala@mun.ca>
2020-11-15 22:23:56 -0700

    Added Test package for unit testing

---
+++ src/test/java/TempTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class TempTest {
+
+    @Test
+    public void testAddition() {
+        int expectedAnswer = 3 + 3;
+        Assert.assertEquals(expectedAnswer, 6);
+    }
+}
