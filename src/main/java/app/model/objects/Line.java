04693c801302f401da91ebdbf1cdc27c4ffb8e68
ShivMUN <53536872+ShivMUN@users.noreply.github.com>
2020-11-27 17:44:39 -0330

    added draw line

--- src/main/java/app/model/objects/Line.java
+++ src/main/java/app/model/objects/Line.java
@@ -1,6 +1,9 @@
 package app.model.objects;
 
+import java.util.Map;
+
 import app.interfaces.DrawingAdapterI;
+import app.model.attributes.AttributeLabel;
 import app.model.attributes.EndX;
 import app.model.attributes.EndY;
 import app.model.attributes.FillColor;
@@ -27,13 +30,31 @@ public class Line extends Objects {
     @Override
     public Boolean locatedAt(double x, double y) {
         // TODO Auto-generated method stub
-        return null;
+        Map<String, String> attributes = this.getAttributes();
+        double start_x = Double.parseDouble(attributes.get(AttributeLabel.X_POSITION.getLabel()));
+        double start_y = Double.parseDouble(attributes.get(AttributeLabel.Y_POSITION.getLabel()));
+        double end_x = Double.parseDouble(attributes.get(AttributeLabel.END_X.getLabel()));
+        double end_y = Double.parseDouble(attributes.get(AttributeLabel.END_Y.getLabel()));
+        double i = Math.sqrt(Math.pow((x - start_x), 2) + Math.pow((y - start_y), 2));
+        double j = Math.sqrt(Math.pow((x - end_x), 2) + Math.pow((y - end_y), 2));
+        double k = Math.sqrt(Math.pow((start_x - end_x), 2) + Math.pow((start_x - end_y), 2));
+        
+        return i + j == k ? true : false;
     }
 
     @Override
     public void draw(DrawingAdapterI drawingAdapter) {
-        // TODO Auto-generated method stub
+        
+        Map<String, String> attributes = this.getAttributes();
+        double start_x = Double.parseDouble(attributes.get(AttributeLabel.X_POSITION.getLabel()));
+        double start_y = Double.parseDouble(attributes.get(AttributeLabel.Y_POSITION.getLabel()));
+        double end_x = Double.parseDouble(attributes.get(AttributeLabel.END_X.getLabel()));
+        double end_y = Double.parseDouble(attributes.get(AttributeLabel.END_Y.getLabel()));
 
+        drawingAdapter.SetLineWidth(Double.parseDouble(attributes.get(AttributeLabel.STROKE_WIDTH.getLabel())));
+        drawingAdapter.SetStrokeColor(attributes.get(AttributeLabel.STROKE_COLOR.getLabel()));
+
+        drawingAdapter.drawLine(start_x, start_y, end_x, end_y);
     }
 }
 
