9469117d5c658974a65411421b78668e40b0efcd
disha bodiwala <dkbodiwala@mun.ca>
2020-11-28 00:34:49 -0700

    Added comments explaining all the attributes in the application

--- src/main/java/app/model/attributes/Width.java
+++ src/main/java/app/model/attributes/Width.java
@@ -1,5 +1,14 @@
 package app.model.attributes;
 
+/**
+ * @author Team Alfa
+ * @declaration “This file was prepared by members of Team Alfa. It was completed by group members alone.”
+ *
+ * Class : This class represents Width attribute.
+ * The value of this attribute determines the object's width.
+ * If no data is provided constructor, then the default value will be 100.
+ */
+
 public class Width extends Attributes {
 
     public static final String DEFAULT_DATA = "100";
