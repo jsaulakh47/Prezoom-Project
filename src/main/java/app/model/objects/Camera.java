d101d758fa424151ce7d109e76bf7275cd2d214a
ShivMUN <53536872+ShivMUN@users.noreply.github.com>
2020-11-29 18:15:25 -0330

    Added comment to all object classes

--- src/main/java/app/model/objects/Camera.java
+++ src/main/java/app/model/objects/Camera.java
@@ -8,11 +8,26 @@ import app.model.attributes.Height;
 import app.model.attributes.StrokeColor;
 import app.model.attributes.Width;
 
+/**
+ * @author Team Alfa
+ * @declaration “This file was prepared by members of Team Alfa. It was completed by group members alone.”
+ * 
+ * Class: This class represents the Camera object and sets the attributes required for the same. 
+ */
+
 public class Camera extends Objects{
 
     private static final String DEFAULT_WIDTH = "1080";
     private static final String DEFAULT_HEIGHT = "720";
     public static final String STROKE_COLOR = "#FF0000";
+
+    /**
+     * This constructor is used to add attributes and sets default value of the attributes of the Camera object.
+     * @param x - It is x-coordinates on canvas.
+     * @param y - It is y-coordinates on canvas.
+     * @param width - It is width of the Camera.
+     * @param height - It is height of the Camera.
+     */
 
     public Camera(String x, String y, String width, String height) {
         super(x, y, ObjectType.CAMERA.getType());
@@ -21,13 +36,23 @@ public class Camera extends Objects{
         addAttribute(new StrokeColor(STROKE_COLOR));
     }
 
+    /**
+     * This constructor overloads the constructor.
+     * @param x - It is x-coordinates on canvas.
+     * @param y - It is y-coordinates on canvas.
+     */
+
     public Camera(String x, String y) {
         this(x, y, DEFAULT_WIDTH, DEFAULT_HEIGHT);
     }
+
+
     
     public Camera() {
         this("0", "0", DEFAULT_WIDTH, DEFAULT_HEIGHT);
     }
+
+    
 
     @Override
     public boolean locatedAt(double x, double y) {
--- src/main/java/app/model/objects/TextArea.java
+++ src/main/java/app/model/objects/TextArea.java
@@ -10,11 +10,27 @@ import app.model.attributes.StrokeColor;
 import app.model.attributes.StrokeWidth;
 import java.util.Map;
 
+/**
+ * @author Team Alfa
+ * @declaration “This file was prepared by members of Team Alfa. It was completed by group members alone.”
+ * 
+ * Class: This class represents the Text-Area object and sets the attributes required for the same. 
+ */
+
 public class TextArea extends Objects {
 
     public static final String DEFAULT_TEXT = "Alfa";
     public static final String DEFAULT_WIDTH = "100";
     public static final String DEFAULT_HEIGHT = "100";
+
+    /**
+     * This constructor is used to add attributes and sets default value of the attributes of the Text-Area object.
+     * @param x - It is x-coordinates on canvas.
+     * @param y - It is y-coordinates on canvas.
+     * @param text - It is the text input.
+     * @param width - It is width of the text box.
+     * @param height - It is height of the text box.
+     */
 
     public TextArea(String x, String y, String text, String width, String height) {
         super(x, y, ObjectType.TEXT_AREA.getType());
@@ -26,9 +42,22 @@ public class TextArea extends Objects {
         addAttribute(new StrokeColor());
     }
 
+    /**
+     * This constructor overloads the constructor.
+     * @param x - It is x-coordinates on canvas.
+     * @param y - It is y-coordinates on canvas.
+     */
+
     public TextArea(String x, String y) {
         this(x, y, DEFAULT_TEXT, DEFAULT_WIDTH, DEFAULT_HEIGHT);
     }
+
+    /**
+     * This sub-routine is used to locate the object on the canvas.
+     * @param x - It is x-coordinates on canvas.
+     * @param y - It is y-coordinates on canvas. 
+     * @return boolean 
+     */
 
     @Override
     public boolean locatedAt(double x, double y) {
@@ -41,6 +70,12 @@ public class TextArea extends Objects {
 
         return (x >= xPos && x <= (xPos + width) && y >= yPos && y <= (yPos + height)) ? true : false;
     }
+
+    /**
+     * This sub-routine draws the Text-Area using the drawingAdapter.
+     * 
+     * @param DrawingAdapterI drawingAdapter - object of DrawingAdapterI
+     */
 
     @Override
     public void draw(DrawingAdapterI drawingAdapter) {
